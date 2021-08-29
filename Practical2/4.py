f20fd8658a63794d2e7b18b5d182bb1c49d8beab
Bhanwar Lal Choudhary <87200474+blc094@users.noreply.github.com>
2021-08-29 09:48:33 +0530

    Add files via upload

---
+++ Practical2/4.py
@@ -0,0 +1,23 @@
+a = 94
+print(a, "type is", type(a)) ###############    int
+
+b = 94.94
+print(b, "type is", type(b)) ###############    float
+
+c = 94 + 94j
+print(c, "type is" ,type(c)) ###############    complex
+
+d = [0,1,2,3,4,5,6,7,8,9]
+print(d, "type is", type(d)) ###############    list is an unordered  sequence and mutable
+
+e = (94,'Bhanwar',1+2j)
+print(e,"type is", type(e)) ###############     tuple are immutable and orderd sequence
+
+f = "Jai Hind"
+print(f, "type is", type(f)) ###############    string is sequence of unicode characters
+
+g = {9,4,7,3,6,2,5,1,8,0}
+print(g, "type is", type(g)) ###############    set is an unordered collection of unique items
+
+h = {1:'value','key':1}
+print(h, 'type is', type(h)) ###############    dictionary is an unordered collection of key value pairs
